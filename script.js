f2c2579b12889acf8bec2d6c2c2f066702bfef0b
Matt Pestridge <mpestridge1986@gmail.com>
2016-10-17 15:59:53 -0600

    created funtionality for stoplight

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict"
 /*
   Write JS to make this stoplight work.
 
@@ -8,3 +9,64 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var button = document.querySelector("stopButton");
+
+stopButton.addEventListener("mouseenter", function(){
+  console.log("Entered " + this.innerHTML + " button");
+});
+
+var button = document.querySelector("slowButton");
+
+slowButton.addEventListener("mouseenter", function(){
+  console.log("Entered " + this.innerHTML + " button");
+});
+
+var button = document.querySelector("goButton");
+
+goButton.addEventListener("mouseenter", function(){
+  console.log("Entered " + this.innerHTML + " button");
+});
+
+var button = document.querySelector("stopButton");
+
+stopButton.addEventListener("mouseout", function(){
+  console.log("Left " + this.innerHTML + " button");
+});
+
+var button = document.querySelector("slowButton");
+
+slowButton.addEventListener("mouseout", function(){
+  console.log("Left " + this.innerHTML + " button");
+});
+
+var button = document.querySelector("goButton");
+
+goButton.addEventListener("mouseout", function(){
+  console.log("Left " + this.innerHTML + " button");
+});
+
+var button = document.getElementsByClassName("button");
+
+var logText = function(event) {
+  console.log(event.target.textContent);
+}
+
+controls.addEventListener("click", logText)
+
+stopButton.addEventListener("click" , function() {
+  document.getElementById("stopLight").style.backgroundColor= "red";
+  document.getElementById("slowLight").style.backgroundColor= "black";
+  document.getElementById("goLight").style.backgroundColor= "black";
+});
+
+slowButton.addEventListener("click" , function() {
+  document.getElementById("slowLight").style.backgroundColor= "yellow";
+  document.getElementById("stopLight").style.backgroundColor= "black";
+  document.getElementById("goLight").style.backgroundColor= "black";
+});
+
+goButton.addEventListener("click" , function() {
+  document.getElementById("goLight").style.backgroundColor= "limegreen";
+  document.getElementById("stopLight").style.backgroundColor= "black";
+  document.getElementById("slowLight").style.backgroundColor= "black";
+});
